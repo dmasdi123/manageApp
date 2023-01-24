0326210c62e0ec1e3b9ee9693f3b8bfa7f018b95
dmasdi123 <febriandimas634@gmail.com>
2023-01-24 20:50:47 +0700

    small changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,18 +14,19 @@ class HomeController extends Controller
     public function index()
     {
         $devname = auth()->user()->name;
+        $status = ['In Progress', 'Complete'];
         return view('dashboard.homepage.index', [
             'title' => 'Home Page',
             'totalproj' => Project::all()->count(),
             'totalrev' => Revision::all()->count(),
             'totalrep' => Report::all()->count(),
-            'devtotalproj' => Project::where(function ($query) use ($devname) {
+            'devtotalproj' => Project::whereIn('status', $status)->where(function ($query) use ($devname) {
                 $query->where('web_dev', $devname);
             })->count(),
-            'devtotalrev' => Revision::where(function ($query) use ($devname) {
+            'devtotalrev' => Revision::whereIn('status', $status)->where(function ($query) use ($devname) {
                 $query->where('web_dev', $devname);
             })->count(),
-            'devtotalrep' => Report::where(function ($query) use ($devname) {
+            'devtotalrep' => Report::whereIn('status', $status)->where(function ($query) use ($devname) {
                 $query->where('web_dev', $devname);
             })->count(),
             'totalcate' => Categories::all()->count(),
